0d2f8b9caf661406a3bdc8a5a90f1c7b2fe898d7
Kuldeep.Prajapati <Kuldeep.Prajapati@ril.com>
2026-03-17 17:22:18 +0530

    Updated the deployment yml

--- Backend/backendDeploy.sh
+++ Backend/backendDeploy.sh
@@ -6,23 +6,23 @@ REGISTRY=""
 TAG="latest"
 
 # List of services
-SERVICES=("audit-module-apis" "auth-apis" "consent-core-apis" "cookie-consent-apis" "grivance-module-apis" "notification-consumer-apis" "notification-module-apis" "patner-portal-apis" "schedular-apis" "translator-apis" "vault-apis" "wso2-cred-generator-apis")
+SERVICES=("audit-module-apis" "auth-apis" "consent-core-apis" "cookie-consent-apis" "grivance-module-apis" "notification-consumer-apis" "notification-module-apis" "patner-portal-apis" "schedular-apis" "translator-apis" "vault-apis" "wso2-cred-generator-apis" "system-registry-apis")
 
 for SERVICE in "${SERVICES[@]}"; do
     echo "--------------------------------------------------"
     echo "Building Docker image for $SERVICE..."
-    docker build -t "$SERVICE:$TAG" "./$SERVICE/deployment"
+    sudo docker build -t "$SERVICE:$TAG" -f "./$SERVICE/deployment/PublicDockerfile" "./$SERVICE"
 
     if [ -n "$REGISTRY" ]; then
         echo "Tagging image for registry..."
-        docker tag "$SERVICE:$TAG" "$REGISTRY/$SERVICE:$TAG"
+        sudo docker tag "$SERVICE:$TAG" "$REGISTRY/$SERVICE:$TAG"
 
         echo "Pushing image to registry $REGISTRY..."
-        docker push "$REGISTRY/$SERVICE:$TAG"
+        sudo docker push "$REGISTRY/$SERVICE:$TAG"
     fi
 
     echo "Deploying $SERVICE to Kubernetes..."
-    kubectl apply -f "./$SERVICE/deployment/jio-dl-deployment.yaml"
+    kubectl apply -f "./$SERVICE/deployment/deployment.yaml"
 done
 
 echo "--------------------------------------------------"
